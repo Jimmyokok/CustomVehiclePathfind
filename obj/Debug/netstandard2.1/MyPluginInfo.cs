a939427840dfbabcf8a7f8a0c6fa9c01512a255d
Jimmyokok <878415277@qq.com>
2023-12-03 12:28:01 +0800

    1.0.2

--- obj/Debug/netstandard2.1/MyPluginInfo.cs
+++
@@ -1,9 +0,0 @@
-namespace CustomVehiclePathfind
-{
-    public static class MyPluginInfo
-    {
-        public const string PLUGIN_GUID = "CustomVehiclePathfind";
-        public const string PLUGIN_NAME = "CustomVehiclePathfind";
-        public const string PLUGIN_VERSION = "1.0.2";
-    }
-}
